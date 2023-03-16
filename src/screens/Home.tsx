c4e21bed6dc7df7a38f03c58d065652ca5406332
Maykon Melo <maykonctba@gmail.com>
2023-03-16 20:04:32 -0300

    Lista de exercicios (Group)

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,9 +1,37 @@
-import { Center, Text } from "native-base";
+import { useState } from "react";
+import { Center, FlatList, HStack, Text, VStack } from "native-base";
+import { HomeHeader } from "../components/HomeHeader";
+import { Group } from "../components/Group";
 
 export function Home() {
+  const [groups, setGroups] = useState(["costas", "TricepS", "omBro", "biCepis"])
+  const [groupSelected, setGroupSelected] = useState("costa")
+
   return (
-    <Center flex={1}>
-      <Text color="white" >Home</Text>
-    </Center>
+    <VStack>
+      <HomeHeader />
+
+      <HStack>
+
+        <FlatList
+          data={groups}
+          keyExtractor={item => item}
+          renderItem={({ item }) => (
+            <Group
+              name={item}
+              isActive={groupSelected === item}
+              onPress={() => setGroupSelected(item)}
+            />
+          )}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          _contentContainerStyle={{ px: 8 }}
+          my={10}
+          maxH={10}
+
+        />
+
+      </HStack>
+    </VStack>
   )
 }
