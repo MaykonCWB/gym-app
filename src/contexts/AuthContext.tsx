2cab520ae94eda368cf72d768444bdb06b10fe76
Maykon Melo <maykonctba@gmail.com>
2023-04-11 15:31:10 -0300

    Token: Criação,Armazenamento,Busca,Envio

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -31,12 +31,12 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
     setUser(userData)
   }
 
-  async function storageUserAndTokenSave(userData: UserDTO, token: string) {
+  async function storageUserAndTokenSave(userData: UserDTO, token: string, refresh_token: string) {
     try {
       setIsLoadUserStorageData(true)
 
       await storageUserSave(userData)
-      await storageAuthTokenSave(token)
+      await storageAuthTokenSave({ token, refresh_token })
 
     } catch (error) {
       throw error
@@ -51,8 +51,8 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
     try {
       const { data } = await api.post("/sessions", { email, password })
 
-      if (data.user && data.token) {
-        await storageUserAndTokenSave(data.user, data.token)
+      if (data.user && data.token && data.refresh_token) {
+        await storageUserAndTokenSave(data.user, data.token, data.refresh_token)
         userAndTokenUpdate(data.user, data.token)
       }
     } catch (error) {
@@ -93,7 +93,7 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
       setIsLoadUserStorageData(true)
 
       const userLogged = await storageUserGet()
-      const token = await storageAuthTokenGet()
+      const { token } = await storageAuthTokenGet()
 
       if (token && userLogged) {
         userAndTokenUpdate(userLogged, token)
@@ -109,6 +109,14 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
   useEffect(() => {
     loaderUserData()
   }, [])
+
+  useEffect(() => {
+    const subscribe = api.registerInterceptTokenManager(signOut)
+
+    return () => {
+      subscribe()
+    }
+  }, [signOut],)
 
   return (
     <AuthContext.Provider value={{
--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,13 +1,14 @@
 import { useCallback, useState } from "react";
+import { useFocusEffect } from "@react-navigation/native";
 import { VStack, Text, SectionList, Heading, useToast } from "native-base";
 
-import { HistoryCard } from "@components/HistoryCard";
-import { ScreenHeader } from "@components/ScreenHeader";
-import { AppError } from "@utils/AppError";
-import { isLoading } from "expo-font";
 import { api } from "@services/api";
-import { useFocusEffect } from "@react-navigation/native";
+import { AppError } from "@utils/AppError";
 import { HistoryByDateDTO } from "@dtos/HistoryByDayDTO";
+
+import { ScreenHeader } from "@components/ScreenHeader";
+import { HistoryCard } from "@components/HistoryCard";
+import { Loading } from "@components/Loading";
 
 export function History() {
   const [isLoading, setIsLoading] = useState(true)
@@ -44,25 +45,29 @@ export function History() {
     <VStack flex={1}>
       <ScreenHeader title="Histórico de Exercício" />
 
-      <SectionList
-        sections={exercises}
-        keyExtractor={item => item.id}
-        renderItem={({ item }) => <HistoryCard data={item} />}
-        renderSectionHeader={({ section }) => (
-          <Heading fontFamily="heading" color="gray.200" fontSize="md" mt={10} mb={3}>
-            {section.title}
-          </Heading>
-        )}
-        px={8}
-        contentContainerStyle={exercises.length === 0 && { flex: 1, justifyContent: "center" }}
-        ListEmptyComponent={() => (
-          <Text color="gray.100" textAlign="center" fontSize="sm" >
-            Não há exercícios registrados ainda. {"\n"}
-            Vamos fazer exercícios hoje?
-          </Text>
-        )}
-        showsVerticalScrollIndicator={false}
-      />
+      {
+        isLoading ? <Loading /> :
+
+          <SectionList
+            sections={exercises}
+            keyExtractor={item => item.id}
+            renderItem={({ item }) => <HistoryCard data={item} />}
+            renderSectionHeader={({ section }) => (
+              <Heading fontFamily="heading" color="gray.200" fontSize="md" mt={10} mb={3}>
+                {section.title}
+              </Heading>
+            )}
+            px={8}
+            contentContainerStyle={exercises.length === 0 && { flex: 1, justifyContent: "center" }}
+            ListEmptyComponent={() => (
+              <Text color="gray.100" textAlign="center" fontSize="sm" >
+                Não há exercícios registrados ainda. {"\n"}
+                Vamos fazer exercícios hoje?
+              </Text>
+            )}
+            showsVerticalScrollIndicator={false}
+          />
+      }
     </VStack>
   )
 }
